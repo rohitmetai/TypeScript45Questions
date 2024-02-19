741fc90fddf8764a2dabb87c64ec183349232284
rohitmetai <134150206+rohitmetai@users.noreply.github.com>
2024-02-20 02:29:36 +0500

    Add files via upload

---
+++ q8.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+console.log('------------------');
+console.log(5 + 3);
+console.log('------------------');
+console.log(10 - 2);
+console.log('------------------');
+console.log(4 * 2);
+console.log('------------------');
+console.log(16 / 2);
