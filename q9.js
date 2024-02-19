b47d970282763a3efbf2606631804207736cea4a
rohitmetai <134150206+rohitmetai@users.noreply.github.com>
2024-02-20 02:29:55 +0500

    Add files via upload

---
+++ q9.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var fav_number = 404;
+var message = 'My favourite number is ' + fav_number;
+console.log(message);
---
+++ q9.ts
@@ -0,0 +1,5 @@
+var fav_number:number=404
+var message:string='My favourite number is '+fav_number
+console.log(message);
+
+export{}
