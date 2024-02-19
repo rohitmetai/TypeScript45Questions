c1aa3e1e353898bbbbef650147099d0f69cb56e1
rohitmetai <134150206+rohitmetai@users.noreply.github.com>
2024-02-20 02:30:14 +0500

    Add files via upload

---
+++ q10.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+console.log('comments');
+//ROHIT KUMAR , CURRENT DATE: 18/2/2024
+//Initializing a number type variable with value 404, named as "fav_person"  
+var fav_number = 404;
+var message = 'My favourite number is ' + fav_number; //number revealing message
+console.log(message); //displaying the message on console
---
+++ q10.ts
@@ -0,0 +1,7 @@
+console.log('comments');
+//ROHIT KUMAR , CURRENT DATE: 18/2/2024
+//Initializing a number type variable with value 404, named as "fav_person"  
+var fav_number:number=404 
+var message:string='My favourite number is '+fav_number //number revealing message
+console.log(message);//displaying the message on console
+export{}
