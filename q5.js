a5935b4e94e0e3f2b133c47a31400f6fae328798
rohitmetai <134150206+rohitmetai@users.noreply.github.com>
2024-02-20 02:28:38 +0500

    Add files via upload

---
+++ q5.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var quote = '"Everything you can imagine is real."';
+var famous_person = 'Pablo Picasso';
+var message = famous_person + ' once said,' + quote;
+console.log(message);
---
+++ q5.ts
@@ -0,0 +1,7 @@
+
+var quote:string='"Everything you can imagine is real."'
+var famous_person:string='Pablo Picasso'
+var message:string=famous_person+' once said,'+quote
+console.log(message);
+
+export{}
